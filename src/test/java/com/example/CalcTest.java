de0aa448c8d00677759c978ea96a4b7d8c8ea1f0
tvtvtv9810 <tvtvtv9810@gmail.com>
2022-05-27 13:47:55 +0900

    github-actionテスト用：Testエラーになる

--- src/test/java/com/example/CalcTest.java
+++ src/test/java/com/example/CalcTest.java
@@ -23,7 +23,7 @@ public class CalcTest {
     public void testSum2(){
         
         // ■ 入力
-        final int[] input = {1,2,3,4,5,6,7,8,9,10,11};
+        final int[] input = {1,2,3,4,5,6,7,8,9,10,11,12};
         // ■ 期待値
         final var expected = 66;
         // ■ 実行
